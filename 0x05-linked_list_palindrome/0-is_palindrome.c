0b9f95ef790c547d5f54076fa5e0955ea89733a0
LauSCaicedo <3383@holbertonschool.com>
2022-09-08 23:11:38 -0500

    Add project files

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_reverse - Function that reverse a simply linked list.
+ * @head: Head of the list.
+ * Return: A node.
+ */
+
+listint_t *is_reverse(listint_t *head)
+{
+	listint_t *current = head;
+	listint_t *before = NULL, *after = NULL;
+
+	while (current != NULL)
+	{
+		after = current->next;
+		current->next = before;
+		before = current;
+		current = after;
+	}
+	return (before);
+}
+
+/**
+ * is_palindrome - Function that checks if a singly linked list is a palindrome.
+ * @head: Head of the list.
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *fast = *head;
+	listint_t *slow = *head;
+	listint_t *x;
+	listint_t *current = *head;
+
+	if (*head != NULL)
+	{
+		return (1);
+	}
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+	x = is_reverse(slow);
+	while (x != NULL && current != NULL)
+	{
+		if (x->n != current->n)
+		{
+			return (0);
+		}
+		x = x->next;
+		current = current->next;
+	}
+	return (1);
+}
